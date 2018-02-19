fbda3f981584c7a6fb5ac918ff0fef30d12968ea
siddiqabu <35294835+siddiqabu@users.noreply.github.com>
2018-02-19 15:50:49 +0530

    Create infinite.py

---
+++ infinite.py
@@ -0,0 +1,18 @@
+def countd(l,n):
+	(max,min)=(0,9999)
+	for i in range(n):
+		if max<l[i]:
+			max=l[i]
+		if min>l[i]:
+			min=l[i]
+	print(min,max)
+def main():
+	try:
+		l=[]
+		n=int(input())
+		for i in range(n):
+			l.append(int(input()))
+		countd(l,n)
+	except:
+		print('infinite')
+main()
