2a7508f0e0bee16b836c9bc474787d10d5642fd4
siddiqabu <35294835+siddiqabu@users.noreply.github.com>
2018-02-19 15:57:08 +0530

    Create power.java

---
+++ power.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class avg{
+     public static void main(String aa[]){
+     int b,f=0;
+     Scanner s=new Scanner(System.in);
+     a=s.nextInt();
+   for(int i=1;i<=a;i++){
+       f=f+i;
+   }
+     int g=f/b;
+     System.out.println(g);
+     
+
+    }
+}
